9b08f3da9c035dad6c6881795e04d86a35dcd658
Nicolas Dhers <nicolas@rkcreation.fr>
2019-10-28 10:20:41 +0100

    reset config theme + add stack example yml

--- config.inc.php
+++ config.inc.php
@@ -7,7 +7,7 @@ $cfg['SaveDir'] = '';
 
 /* Theme Metro */
 $cfg['ThemeManager'] = false;
-$cfg['ThemeDefault'] = 'pmahomme';//isset($_ENV['CUSTOM_THEME']) ? $_ENV['CUSTOM_THEME'] : 'fallen';
+$cfg['ThemeDefault'] = isset($_ENV['CUSTOM_THEME']) ? $_ENV['CUSTOM_THEME'] : 'fallen';
 
 /* Login validity */
 $cfg['LoginCookieValidity'] = isset($_ENV['LOGIN_EXPIRATION']) ? $_ENV['LOGIN_EXPIRATION'] : 24 * 3600;
